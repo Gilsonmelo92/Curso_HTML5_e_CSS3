60ec3fe18e8b5b1a44acc1e23c038bc134131922
Gilson Melo <79469113+Gilsonmelo92@users.noreply.github.com>
2021-12-20 09:33:37 -0300

    Initial commit

---
+++ 07_significados das tags/Principais Tags no HTML/16_tag_script.js
@@ -0,0 +1,3 @@
+/* alert("C , o browzer já esta lendo o conteúda tag script, nessa etapa. ") */
+
+console.log(document.querySelector("h1"));
---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso_HTML5_e_CSS3
+ Curso HTML5 e CSS3 - Curso em vídeo 
